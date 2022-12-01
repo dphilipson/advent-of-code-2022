2d863da19c1c72bda2baa809c953b9bd2c0e6055
David Philipson <david.philipson@gmail.com>
2022-11-30 21:38:07 -0800

    Day 1

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,11 +1,20 @@
 use crate::harness::input::RawInput;
 
-pub fn solve_part1(input: RawInput) -> usize {
-    input.as_str();
-    todo!()
+pub fn solve_part1(input: RawInput) -> u32 {
+    let groups = input.grouped_lines(|line| line.single::<u32>());
+    groups
+        .into_iter()
+        .map(|group| group.into_iter().sum())
+        .max()
+        .unwrap()
 }
 
-pub fn solve_part2(input: RawInput) -> usize {
-    input.as_str();
-    todo!()
+pub fn solve_part2(input: RawInput) -> u32 {
+    let groups = input.grouped_lines(|line| line.single::<u32>());
+    let mut sums: Vec<u32> = groups
+        .into_iter()
+        .map(|group| group.into_iter().sum())
+        .collect();
+    sums.sort();
+    sums.into_iter().rev().take(3).sum()
 }
