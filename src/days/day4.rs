7f6930364d9da68d3eea1eaae81e41c1f7d1d0a1
David Philipson <david.philipson@gmail.com>
2022-12-03 21:07:06 -0800

    Day 4

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -1,11 +1,26 @@
 use crate::harness::input::RawInput;
+use crate::regex;
 
 pub fn solve_part1(input: RawInput) -> usize {
-    input.as_str();
-    todo!()
+    input
+        .per_line(|line| {
+            line.parse_with_regex::<(u32, u32, u32, u32)>(regex!(r"(\d+)-(\d+),(\d+)-(\d+)"))
+        })
+        .into_iter()
+        .filter(|&(start1, end1, start2, end2)| {
+            (start1 <= start2 && end1 >= end2) || (start2 <= start1 && end2 >= end1)
+        })
+        .count()
 }
 
 pub fn solve_part2(input: RawInput) -> usize {
-    input.as_str();
-    todo!()
+    input
+        .per_line(|line| {
+            line.parse_with_regex::<(u32, u32, u32, u32)>(regex!(r"(\d+)-(\d+),(\d+)-(\d+)"))
+        })
+        .into_iter()
+        .filter(|&(start1, end1, start2, end2)| {
+            (start1 <= start2 && end1 >= start2) || (start2 <= start1 && end2 >= start1)
+        })
+        .count()
 }
