8863b1cd5ac52b74ce63ff74f1b2f0a789b12b07
David Philipson <david.philipson@gmail.com>
2022-12-05 21:05:49 -0800

    Day 6

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -1,11 +1,22 @@
 use crate::harness::input::RawInput;
+use std::collections::HashSet;
 
 pub fn solve_part1(input: RawInput) -> usize {
-    input.as_str();
-    todo!()
+    solve(input, 4)
 }
 
 pub fn solve_part2(input: RawInput) -> usize {
-    input.as_str();
-    todo!()
+    solve(input, 14)
+}
+
+fn solve(input: RawInput, window_size: usize) -> usize {
+    input
+        .as_str()
+        .as_bytes()
+        .windows(window_size)
+        .enumerate()
+        .find(|&(_, bytes)| bytes.iter().collect::<HashSet<_>>().len() == window_size)
+        .unwrap()
+        .0
+        + window_size
 }
