9a647fa3510e469c5c13755000676f279b8a2b95
David Philipson <david.philipson@gmail.com>
2022-12-08 09:52:06 -0800

    Even faster Day 8 by reusing Vecs

--- src/days/day8.rs
+++ src/days/day8.rs
@@ -4,40 +4,52 @@ use std::cmp;
 
 pub fn solve_part1(input: RawInput) -> usize {
     let heights = Grid::parse_digits(input.as_str());
+    let mut views = initialize_views(&heights);
     heights
         .indices()
         .filter(|&ij| {
             let height = heights[ij];
-            get_views(&heights, ij)
-                .into_iter()
-                .any(|mut view| view.all(|h| h < height))
+            fill_views(&mut views, &heights, ij);
+            views.iter().any(|view| view.iter().all(|&h| h < height))
         })
         .count()
 }
 
 pub fn solve_part2(input: RawInput) -> usize {
     let heights = Grid::parse_digits(input.as_str());
+    let mut views = initialize_views(&heights);
     heights
         .indices()
         .map(|ij| {
             let height = heights[ij];
-            get_views(&heights, ij)
-                .into_iter()
-                .map(|view| cmp::min(view.len(), view.take_while(|&h| h < height).count() + 1))
+            fill_views(&mut views, &heights, ij);
+            views
+                .iter()
+                .map(|view| {
+                    cmp::min(
+                        view.len(),
+                        view.iter().take_while(|&&h| h < height).count() + 1,
+                    )
+                })
                 .product()
         })
         .max()
         .unwrap()
 }
 
-fn get_views(
-    heights: &Grid<u32>,
-    [i, j]: [usize; 2],
-) -> [Box<dyn ExactSizeIterator<Item = u32> + '_>; 4] {
+fn initialize_views(heights: &Grid<u32>) -> [Vec<u32>; 4] {
     [
-        Box::new((0..i).rev().map(move |i2| heights[[i2, j]])),
-        Box::new((i + 1..heights.nrows()).map(move |i2| heights[[i2, j]])),
-        Box::new((0..j).rev().map(move |j2| heights[[i, j2]])),
-        Box::new((j + 1..heights.ncols()).map(move |j2| heights[[i, j2]])),
+        Vec::with_capacity(heights.nrows() - 1),
+        Vec::with_capacity(heights.nrows() - 1),
+        Vec::with_capacity(heights.ncols() - 1),
+        Vec::with_capacity(heights.ncols() - 1),
     ]
+}
+
+fn fill_views(views: &mut [Vec<u32>; 4], heights: &Grid<u32>, [i, j]: [usize; 2]) {
+    views.iter_mut().for_each(|v| v.clear());
+    views[0].extend((0..i).rev().map(move |i2| heights[[i2, j]]));
+    views[1].extend((i + 1..heights.nrows()).map(move |i2| heights[[i2, j]]));
+    views[2].extend((0..j).rev().map(move |j2| heights[[i, j2]]));
+    views[3].extend((j + 1..heights.ncols()).map(move |j2| heights[[i, j2]]));
 }
