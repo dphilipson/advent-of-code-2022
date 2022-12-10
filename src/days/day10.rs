056b5bc011db4f4356371a0f32607e8e3ba18f83
David Philipson <david.philipson@gmail.com>
2022-12-09 22:33:28 -0800

    Cleaned up Day 10

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,50 +1,92 @@
 use crate::harness::input::RawInput;
+use std::num::ParseIntError;
+use std::str::FromStr;
+use Instruction::{Addx, Noop};
 
 pub fn solve_part1(input: RawInput) -> i32 {
-    let mut total_time: i32 = 0;
-    let mut time = 20;
-    let mut x: i32 = 1;
-    let mut sum = 0;
-    input.per_line(|line| line.split_whitespace::<String>())
-        .for_each(|parts| {
-            time += parts.len();
-            total_time += parts.len() as i32;
-            if time >= 40 {
-                sum += x * (total_time / 20) * 20;
-                time -= 40;
-            }
-            if parts.len() == 2 {
-                x += parts[1].parse::<i32>().unwrap();
-            }
-        });
-    sum
+    let instructions = input.per_line(|line| line.single()).collect();
+    let mut state = State::new(instructions);
+    let mut result = 0;
+    while !state.is_done() {
+        let x = state.x;
+        state.advance();
+        if (state.cycle - 20) % 40 == 0 {
+            result += state.cycle * x;
+        }
+    }
+    result
 }
 
-pub fn solve_part2(input: RawInput) -> usize {
-    let mut time = 0;
-    let mut x: i32 = 1;
-    let mut i = 0;
-    let mut executing_add = false;
+pub fn solve_part2(input: RawInput) -> u8 {
+    let instructions = input.per_line(|line| line.single()).collect();
+    let mut state = State::new(instructions);
     let mut chars = vec![];
-    let instructions: Vec<_> = input.per_line(|line| line.split_whitespace::<String>()).collect();
-    while i < instructions.len() {
-        chars.push(if (time - x).abs() <= 1 { '#' } else { '.' });
-        let instruction = &instructions[i];
-        if instruction.len() == 1 {
-            i += 1;
-        } else {
-            if executing_add {
-                x += instruction[1].parse::<i32>().unwrap();
-                i += 1;
-            }
-            executing_add = !executing_add;
-        }
-        if time == 40 {
+    while !state.is_done() {
+        let x = state.x;
+        let column = state.cycle % 40;
+        let char = if (column - x).abs() <= 1 { '█' } else { ' ' };
+        chars.push(char);
+        if column == 39 {
             chars.push('\n');
-            time = 0;
         }
-        time += 1;
+        state.advance();
     }
     println!("{}", chars.into_iter().collect::<String>());
     0
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Instruction {
+    Addx(i32),
+    Noop,
+}
+
+impl FromStr for Instruction {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s.split_once(' ') {
+            Some((_, n)) => Addx(n.parse()?),
+            None => Noop,
+        })
+    }
+}
+
+#[derive(Debug)]
+struct State {
+    instructions: Vec<Instruction>,
+    instruction_index: usize,
+    cycle: i32,
+    x: i32,
+    is_adding: bool,
+}
+
+impl State {
+    fn new(instructions: Vec<Instruction>) -> Self {
+        Self {
+            instructions,
+            instruction_index: 0,
+            cycle: 0,
+            x: 1,
+            is_adding: false,
+        }
+    }
+
+    fn advance(&mut self) {
+        self.cycle += 1;
+        match self.instructions[self.instruction_index] {
+            Addx(n) => {
+                if self.is_adding {
+                    self.x += n;
+                    self.instruction_index += 1;
+                }
+                self.is_adding = !self.is_adding
+            }
+            Noop => self.instruction_index += 1,
+        }
+    }
+
+    fn is_done(&self) -> bool {
+        self.instruction_index == self.instructions.len()
+    }
 }
