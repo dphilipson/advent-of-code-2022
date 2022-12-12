04a966bbe4bb4d27fab0f04b23d562ae2de2344f
David Philipson <david.philipson@gmail.com>
2022-12-11 21:27:11 -0800

    Day 12

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,11 +1,34 @@
 use crate::harness::input::RawInput;
+use crate::util::grid::Grid;
+use crate::util::search::bfs;
 
 pub fn solve_part1(input: RawInput) -> usize {
-    input.as_str();
-    todo!()
+    solve(input, b'S')
 }
 
 pub fn solve_part2(input: RawInput) -> usize {
-    input.as_str();
-    todo!()
+    solve(input, b'a')
+}
+
+fn solve(input: RawInput, goal: u8) -> usize {
+    let heights = Grid::parse_bytes(input.as_str());
+    let result = bfs::search(
+        heights.indices().find(|&ij| heights[ij] == b'E').unwrap(),
+        |&ij| {
+            heights
+                .orthogonal_neighbors(ij)
+                .filter(|&ij2| get_height(heights[ij]) <= get_height(heights[ij2]) + 1)
+                .collect::<Vec<_>>()
+        },
+        |&ij| heights[ij] == goal,
+    );
+    result.goal_state().unwrap().distance
+}
+
+fn get_height(h: u8) -> u8 {
+    match h {
+        b'S' => b'a',
+        b'E' => b'z',
+        _ => h,
+    }
 }
