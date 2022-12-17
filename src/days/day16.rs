ed6b61ff7f4aa79e6be9138e45e3cec2c67c4eae
David Philipson <david.philipson@gmail.com>
2022-12-17 14:21:18 -0800

    Much improved Day 16, add arrayvec

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -1,201 +1,83 @@
 use crate::harness::input::RawInput;
 use crate::regex;
 use crate::util::re;
+use crate::util::re::Str;
+use arrayvec::ArrayString;
 use ndarray::{Array2, Array3};
-use std::collections::{HashMap, VecDeque};
 use std::str::FromStr;
 use std::{cmp, error};
 
-// This code is basically unreadable. Don't expect a good solution ahead.
-
 pub fn solve_part1(input: RawInput) -> u32 {
-    let valves_by_label = parse_valves(input);
-    let mut best_by_state = HashMap::<State, (u32, u32)>::new();
-    let mut pending = VecDeque::from([State {
-        location: [b'A', b'A'],
-        open_valves: vec![],
-        pressure: 0,
-        pressure_rate: 0,
-        time: 0,
-    }]);
-    while let Some(state) = pending.pop_front() {
-        if let Some(&(best_time, best_pressure)) = best_by_state.get(&state) {
-            if state.time >= best_time
-                && state.pressure <= best_pressure + (state.time - best_time) * state.pressure_rate
-            {
-                continue;
-            }
-            if state.pressure > best_pressure {
-                best_by_state.insert(state.clone(), (state.time, state.pressure));
-            }
-        } else {
-            best_by_state.insert(state.clone(), (state.time, state.pressure));
-        }
-        if state.time == 30 {
-            continue;
-        }
-        let current_location = valves_by_label.get(&state.location).unwrap();
-        let next_pressure = state.pressure + state.pressure_rate;
-        if current_location.flow_rate > 0 && !state.open_valves.contains(&state.location) {
-            let mut open_valves = state.open_valves.clone();
-            open_valves.push(state.location);
-            open_valves.sort();
-            pending.push_back(State {
-                location: state.location,
-                open_valves,
-                pressure: next_pressure,
-                pressure_rate: state.pressure_rate + current_location.flow_rate,
-                time: state.time + 1,
-            });
-        }
-        for &next_location in &current_location.connections {
-            pending.push_back(State {
-                location: next_location,
-                open_valves: state.open_valves.clone(),
-                pressure: next_pressure,
-                pressure_rate: state.pressure_rate,
-                time: state.time + 1,
-            })
-        }
-    }
-    best_by_state
-        .values()
-        .map(|&(_, pressure)| pressure)
+    let layout = parse_layout(input);
+    get_best_scores_by_bitset(&layout, 30)
+        .into_iter()
         .max()
         .unwrap()
 }
 
 pub fn solve_part2(input: RawInput) -> u32 {
-    let max_time = 26;
-    let valves_by_label = parse_valves(input);
-    let mut labels: Vec<_> = valves_by_label.keys().copied().collect();
-    labels.sort();
-    let index_for_label = |label: Label| labels.iter().position(|&l| l == label).unwrap();
-    let mut dists = Array2::from_elem((labels.len(), labels.len()), 10000_u32);
-    for valve in valves_by_label.values() {
-        let i = index_for_label(valve.label);
-        for &connection in &valve.connections {
-            let j = index_for_label(connection);
-            dists[[i, j]] = 1;
-            dists[[j, i]] = 1;
-        }
-    }
-    for i in 0..labels.len() {
-        dists[[i, i]] = 0;
-    }
-    for k in 0..labels.len() {
-        for i in 0..labels.len() {
-            for j in 0..labels.len() {
-                dists[[i, j]] = cmp::min(dists[[i, j]], dists[[i, k]] + dists[[k, j]]);
+    let layout = parse_layout(input);
+    let scores_by_bitset = get_best_scores_by_bitset(&layout, 26);
+    let bitset_max = 1 << layout.n_valves();
+    let mut best = 0;
+    for i in 0..bitset_max {
+        for j in i + 1..bitset_max {
+            if i & j == 0 {
+                let score = scores_by_bitset[i] + scores_by_bitset[j];
+                best = cmp::max(best, score);
             }
         }
     }
-    let mut useful_labels: Vec<_> = valves_by_label
-        .values()
-        .filter(|&valve| valve.flow_rate > 0 || valve.label == [b'A', b'A'])
-        .map(|valve| valve.label)
-        .collect();
-    useful_labels.sort();
-    let get_dist = |label_idx1: usize, label_idx2: usize| {
-        dists[[
-            index_for_label(useful_labels[label_idx1]),
-            index_for_label(useful_labels[label_idx2]),
-        ]]
-    };
-    let useful_flow_rates = useful_labels
-        .iter()
-        .map(|label| valves_by_label.get(label).unwrap().flow_rate)
-        .collect::<Vec<_>>();
-    let pressure_gains_by_subset: Vec<_> = (0..1 << useful_labels.len())
-        .map(|bitset| {
-            (0..useful_labels.len())
-                .map(|i| {
-                    if bitset & (1 << i) > 0 {
-                        useful_flow_rates[i]
-                    } else {
-                        0
-                    }
-                })
-                .sum::<u32>()
-        })
-        .collect();
-    let mut bests: Array3<Option<u32>> = Array3::from_elem(
-        (max_time + 1, useful_labels.len(), 1 << useful_labels.len()),
-        None,
-    );
-    bests[[0, 0, 1]] = Some(0);
-    for time in 1..=max_time {
-        for label in 0..useful_labels.len() {
-            for bitset in 0..(1 << useful_labels.len()) {
-                if bitset & (1 << label) == 0 {
+    best
+}
+
+fn parse_layout(input: RawInput) -> Layout {
+    let valves: Vec<_> = input.per_line(|line| line.single::<Valve>()).collect();
+    Layout::from_valves(&valves)
+}
+
+fn get_best_scores_by_bitset(layout: &Layout, time_limit: usize) -> Vec<u32> {
+    // bests[[time, location, bitset]] represents the best possible score that
+    // can be obtained by being at `location` at `time` with the valves in
+    // `bitset` opened, and then waiting there until time runs out.
+    let mut bests =
+        Array3::<u32>::default((time_limit + 1, layout.n_valves(), 1 << layout.n_valves()));
+    let mut bests_by_bitset = vec![0_u32; 1 << layout.n_valves()];
+    let mut pending_by_time: Vec<Vec<[usize; 2]>> = vec![vec![]; time_limit];
+    for (i, valve) in layout.valves.iter().enumerate() {
+        let time = valve.distance_from_start + 1;
+        let bitset = 1 << i;
+        let score = valve.flow_rate * (time_limit - time) as u32;
+        bests[[time, i, bitset]] = score;
+        bests_by_bitset[bitset] = score;
+        pending_by_time[time].push([i, bitset]);
+    }
+    for time in 0..time_limit - 1 {
+        for pending_index in 0..pending_by_time[time].len() {
+            // Iterate over index rather than elements to avoid double-borrow.
+            let [i, bitset] = pending_by_time[time][pending_index];
+            for j in 0..layout.n_valves() {
+                if bitset & 1 << j != 0 {
                     continue;
                 }
-                let mut best = None;
-                if time > 0 {
-                    if let Some(idle_prev) = bests[[time - 1, label, bitset]] {
-                        best = Some(idle_prev + pressure_gains_by_subset[bitset]);
-                    }
+                let next_time = time + layout.distance(i, j) + 1;
+                if next_time >= time_limit {
+                    continue;
                 }
-                for label2 in 0..useful_labels.len() {
-                    if label == label2 || bitset & (1 << label2) == 0 {
-                        continue;
-                    }
-                    let time_diff = get_dist(label, label2) as usize + 1;
-                    if time_diff > time {
-                        continue;
-                    }
-                    if time_diff == time && label2 != 0 {
-                        continue;
-                    }
-                    let prev_bitset = bitset & !(1 << label);
-                    if let Some(prev_best) = bests[[time - time_diff, label2, prev_bitset]] {
-                        best = cmp::max(
-                            best,
-                            Some(
-                                prev_best
-                                    + pressure_gains_by_subset[prev_bitset] * time_diff as u32,
-                            ),
-                        );
-                    }
-                }
-                bests[[time, label, bitset]] = best;
+                let next_bitset = bitset | 1 << j;
+                let score = bests[[time, i, bitset]]
+                    + layout.flow_rate(j) * (time_limit - next_time) as u32;
+                let next = [next_time, j, next_bitset];
+                bests[next] = cmp::max(bests[next], score);
+                bests_by_bitset[next_bitset] = cmp::max(bests_by_bitset[next_bitset], score);
+                pending_by_time[next_time].push([j, next_bitset]);
             }
         }
     }
-    let best_by_subset: Vec<_> = (0..1 << useful_labels.len())
-        .map(|bitset| {
-            (0..useful_labels.len())
-                .map(|label| bests[[max_time, label, bitset]])
-                .max()
-                .unwrap()
-        })
-        .collect();
-    let mut best = None;
-    for subset in 0..1 << useful_labels.len() {
-        for subset2 in subset + 1..1 << useful_labels.len() {
-            if subset & subset2 != 1 {
-                continue;
-            }
-            if let Some(a) = best_by_subset[subset] {
-                if let Some(b) = best_by_subset[subset2] {
-                    best = cmp::max(best, Some(a + b));
-                }
-            }
-        }
-    }
-    best.unwrap()
+    bests_by_bitset
 }
 
-#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-struct State {
-    time: u32,
-    location: Label,
-    open_valves: Vec<Label>,
-    pressure: u32,
-    pressure_rate: u32,
-}
-
-type Label = [u8; 2];
+type Label = ArrayString<2>;
 
 #[derive(Debug)]
 struct Valve {
@@ -204,34 +86,95 @@ struct Valve {
     connections: Vec<Label>,
 }
 
-fn parse_valves(input: RawInput) -> HashMap<Label, Valve> {
-    input
-        .per_line(|line| line.single::<Valve>())
-        .map(|valve| (valve.label, valve))
-        .collect()
-}
-
 impl FromStr for Valve {
     type Err = Box<dyn error::Error>;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (name, flow_rate, tunnels): (String, u32, String) = re::parse_with_regex(
+        let (label, flow_rate, Str(tunnels)): (Label, u32, Str) = re::parse_with_regex(
             regex!(r"^Valve (..) has flow rate=(\d+); tunnels? leads? to valves? (.+)$"),
             s,
         )?;
         let connections = tunnels
             .split(", ")
-            .map(|s| {
-                let bytes = s.as_bytes();
-                [bytes[0], bytes[1]]
-            })
+            .map(|s| Label::from(s).unwrap())
             .collect();
-        let name = name.as_bytes();
-        let name = [name[0], name[1]];
         Ok(Self {
-            label: name,
+            label,
             flow_rate,
             connections,
         })
     }
+}
+
+/// `UsefulValves` are valves with nonzero flow. Not useful outside of a `Layout` struct.
+#[derive(Debug)]
+struct UsefulValve {
+    flow_rate: u32,
+    /// Index of this valve when reading from a table of distances.
+    distance_index: usize,
+    distance_from_start: usize,
+}
+
+#[derive(Debug)]
+struct Layout {
+    valves: Vec<UsefulValve>,
+    distances: Array2<usize>,
+}
+
+impl Layout {
+    fn from_valves(valves: &[Valve]) -> Self {
+        let distances = compute_distances(valves);
+        let start_label = Label::from("AA").unwrap();
+        let original_index_of_start = valves.iter().position(|v| v.label == start_label).unwrap();
+        let useful_valves_with_original_index: Vec<_> = valves
+            .iter()
+            .enumerate()
+            .filter(|(_, v)| v.flow_rate > 0)
+            .collect();
+        let valves: Vec<_> = useful_valves_with_original_index
+            .iter()
+            .map(|&(i, valve)| UsefulValve {
+                flow_rate: valve.flow_rate,
+                distance_index: i,
+                distance_from_start: distances[[i, original_index_of_start]],
+            })
+            .collect();
+        Self { valves, distances }
+    }
+
+    fn n_valves(&self) -> usize {
+        self.valves.len()
+    }
+
+    fn flow_rate(&self, i: usize) -> u32 {
+        self.valves[i].flow_rate
+    }
+
+    fn distance(&self, i: usize, j: usize) -> usize {
+        self.distances[[self.valves[i].distance_index, self.valves[j].distance_index]]
+    }
+}
+
+fn compute_distances(valves: &[Valve]) -> Array2<usize> {
+    // Floyd-Warshall algorithm for all-pairs shortest paths
+    let index_for_label = |l: Label| valves.iter().position(|v| v.label == l).unwrap();
+    let mut distances = Array2::<usize>::from_elem((valves.len(), valves.len()), usize::MAX / 2);
+    for i in 0..valves.len() {
+        distances[[i, i]] = 0;
+    }
+    for (i, valve) in valves.iter().enumerate() {
+        for &connection in &valve.connections {
+            let j = index_for_label(connection);
+            distances[[i, j]] = 1;
+        }
+    }
+    for k in 0..valves.len() {
+        for i in 0..valves.len() {
+            for j in 0..valves.len() {
+                distances[[i, j]] =
+                    cmp::min(distances[[i, j]], distances[[i, k]] + distances[[k, j]])
+            }
+        }
+    }
+    distances
 }
--- src/util.rs
+++ src/util.rs
@@ -3,6 +3,5 @@ pub mod grid;
 pub mod ints;
 pub mod nums;
 pub mod re;
-pub mod sized_str;
 pub mod search;
 pub mod string_enum;
