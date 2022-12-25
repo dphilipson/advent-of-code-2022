761c1dea6ca23660e0cf94c5d6c851fce9e7f318
David Philipson <david.philipson@gmail.com>
2022-12-24 21:19:27 -0800

    Day 25

--- src/days/day25.rs
+++ src/days/day25.rs
@@ -1,11 +1,34 @@
 use crate::harness::input::RawInput;
 
-pub fn solve_part1(input: RawInput) -> usize {
-    input.as_str();
+const DIGITS: &[u8] = b"=-012";
+
+pub fn solve_part1(input: RawInput) -> String {
+    let sum: i64 = input
+        .per_line(|line| from_snafu(line.as_str().as_bytes()))
+        .sum();
+    to_snafu(sum)
+}
+
+pub fn solve_part2(_: RawInput) -> usize {
     todo!()
 }
 
-pub fn solve_part2(input: RawInput) -> usize {
-    input.as_str();
-    todo!()
+fn from_snafu(bs: &[u8]) -> i64 {
+    let mut out = 0;
+    for b in bs {
+        out *= 5;
+        out += DIGITS.iter().position(|digit| digit == b).unwrap() as i64 - 2;
+    }
+    out
+}
+
+fn to_snafu(mut n: i64) -> String {
+    let mut out = vec![];
+    while n > 0 {
+        let rem = ((n + 2) % 5) - 2;
+        out.push(DIGITS[(rem + 2) as usize]);
+        n = (n - rem) / 5;
+    }
+    out.reverse();
+    String::from_utf8(out).unwrap()
 }
